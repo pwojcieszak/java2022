8a7debc4e81352f5415053097938fb721e25b26f
pwojcieszak <piotrekw1002@gmail.com>
2022-10-19 17:19:14 +0200

    Initial commmit of branch lab02

---
+++ src/main/java/pl/edu/uj/pesel/PeselValidator.java
@@ -0,0 +1,20 @@
+package pl.edu.uj.pesel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.MessageFormat;
+
+public class PeselValidator {
+
+  private static final Logger logger = LoggerFactory.getLogger(PeselValidator.class);
+
+  public static void main(String[] args) {
+    // TODO should read input file path and write to output file
+    for (String arg : args) {
+      Pesel pesel = new Pesel(arg);
+      boolean valid = Pesel.check(pesel);
+      logger.info(MessageFormat.format("PESEL \"{0}\" is {1}", arg, valid ? "valid" : "invalid"));
+    }
+  }
+}
