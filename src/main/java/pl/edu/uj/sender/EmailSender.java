25fffc1d92c2f97acb9d3675079f82819d08a160
pwojcieszak <piotrekw1002@gmail.com>
2022-11-23 09:45:04 +0100

    Lab03- deleted TODOs

--- src/main/java/pl/edu/uj/sender/EmailSender.java
+++ src/main/java/pl/edu/uj/sender/EmailSender.java
@@ -13,23 +13,16 @@ public class EmailSender implements Sender {
   public void send(Message message, Recipient recipient)
           throws SenderException, InterruptedException {
 
-    // TODO sprawdź czy message jest klasy EmailMessage
-    //  oraz recipient klasy EmailRecipient
-    //  Jeśli nie, throw new SenderException(...)
     if(!((message instanceof EmailMessage) && (recipient instanceof EmailRecipient))){
       throw new SenderException("Typy EmailMessage i/lub EmailRecipient sie nie zgadzaja");
     }
 
-    // TODO wywyołaj walidację wiadomości
     message.validateMessage();
-    // TODO wywolaj walidację odbiorcy
     recipient.validateRecipient();
 
     String bodyMD5 = message.anonymizeMessageBody();
     String anonymizedRecipientAddress = recipient.anonymize();
 
-    // TODO Dodaj wyjątek do sygnatury funkcji (Intelij sam podpowiada)
-    //  Co trzeba zmienić w interfejsie?
     sleep(5000); // sending
 
     /* Use System.out to graphically distinguish sending from logging */
